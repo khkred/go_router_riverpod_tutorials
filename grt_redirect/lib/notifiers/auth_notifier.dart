ddf5ccab51e439968756321e6363761d6a71d089
Harish Kunchala <32921476+khkred@users.noreply.github.com>
2024-05-21 17:22:52 -0700

    grt_redirect: State restoration

--- grt_redirect/lib/notifiers/auth_notifier.dart
+++ grt_redirect/lib/notifiers/auth_notifier.dart
@@ -1,15 +1,37 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthNotifier extends StateNotifier<Map<String, dynamic>> {
-  AuthNotifier() : super({'loggedIn': false, 'role': 'guest'});
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
-  void login(String role) {
-    state = {'loggedIn': true, 'role': role};
-    print('State change to true,  Logged in as $role');
+  AuthNotifier() : super({'loggedIn': false, 'role': 'guest'}) {
+    _restoreState();
   }
 
-  void logout() {
+  Future<void> _restoreState() async {
+    //First read the loggedIn state from the storage
+    final loggedIn = await _storage.read(key: 'loggedIn') == 'true';
+
+    //Next read the role from the storage
+    final role = await _storage.read(key: 'role') ?? 'guest';
+    state = {'loggedIn': loggedIn, 'role': role};
+  }
+
+  Future<void> login(String role) async{
+    state = {'loggedIn': true, 'role': role};
+    print('State change to true,  Logged in as $role');
+
+    //Save the loggedIn state to the storage
+    await _storage.write(key: 'loggedIn', value: 'true');
+    await _storage.write(key: 'role', value: role);
+  }
+
+  Future<void> logout() async{
     state = {'loggedIn': false, 'role': 'guest'};
     print('State change to false, Logged out');
+
+    //Save the loggedOut state to the storage
+    await _storage.write(key: 'loggedIn', value: 'false');
+    await _storage.write(key: 'role', value: 'guest');
   }
 }
