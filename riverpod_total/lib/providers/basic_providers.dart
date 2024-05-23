98c1b6327b06d89db58a42b1cce9860d2fb93ba4
Harish Kunchala <32921476+khkred@users.noreply.github.com>
2024-05-23 12:49:09 -0700

    Riverpod Future and Stream Providers Added

--- riverpod_total/lib/providers/basic_providers.dart
+++ riverpod_total/lib/providers/basic_providers.dart
@@ -1,5 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:riverpod_total/models/post.dart';
 
 final helloWorldProvider = Provider<String>((ref) => 'Hello, World!');
 
-final counterProvider = StateProvider<int>((ref)=> 0);
+final counterProvider = StateProvider<int>((ref) => 0);
+
+final postsProvider = FutureProvider<List<Post>>((ref) async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    List jsonData = json.decode(response.body);
+
+    return jsonData.map((post) => Post.fromJson(post)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
+});
+
+final postsStreamProvider = StreamProvider<List<Post>>((ref) async* {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    List jsonData = json.decode(response.body);
+
+    yield jsonData.map((post) => Post.fromJson(post)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
+
+  await Future.delayed(const Duration(seconds: 5));
+});
